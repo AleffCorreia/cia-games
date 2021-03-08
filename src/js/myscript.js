15176e98aa21ee5b50ab3dd3ac0875674e71532d
Aleff Correia <aleffcorreia@outlook.com>
2021-03-08 11:12:21 -0300

    final commit

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -13,4 +13,21 @@ $('#principal').owlCarousel({
             items:1
         }
     }
-})
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:6
+        }
+    }
+});
